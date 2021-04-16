855f30f6b52ac3d124a4c5b7a6224eefa0ab5c81
AlexisAriege <alexis.generes@gmail.com>
2021-04-16 14:15:55 +0200

    .

--- ChargeDeTravail.c
+++ ChargeDeTravail.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <pthread.h>
+
+
 int main(){
   int a;
   float b=0.5;
   float c=50;
 
-  for(a=0;a<50000;a++){
-    c=c*c*cos(b);
+  for(a=0;a<250000;a++){
+    c=b*b*cos(b);
     b=sin(b)*sqrt(b);
+    c=a;
   }
-  printf("fini");
+  printf("fini %d ca",a);
 
 
   return 0;
--- Lancement.sh
+++ Lancement.sh
@@ -2,6 +2,6 @@
 
 #L'objectif de ce scirpt est de lancer une charge de travail avec l'ordonnancement EDF disponnible sur linux.
 # chrt -d -T <runtime> -D <deadline> -P <period> 0 <command> <command options>
-gcc -o Workload ChargeDeTravail.c -lm
+# gcc -o -lpthread Workload ChargeDeTravail.c -lm
 echo "Compilation terminé"
-./Workload
+#./Workload
