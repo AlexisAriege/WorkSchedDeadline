710ed477a164bb9e96d740a42276262a289b9914
AlexisAriege <alexis.generes@gmail.com>
2021-04-16 12:49:24 +0200

    Commande pour lancer un programme avec ordonnencement EDF

--- Lancement.sh
+++ Lancement.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 #L'objectif de ce scirpt est de lancer une charge de travail avec l'ordonnancement EDF disponnible sur linux.
+# chrt -d -T <runtime> -D <deadline> -P <period> 0 <command> <command options>
+ chrt -d -T <runtime> -D <deadline> -P <period> 0 <command> <command options>
