48c9d6b0f898ef7ae37773547d239b3a924575a5
AlexisAriege <alexis.generes@gmail.com>
2021-04-22 15:38:26 +0200

    Mise à jour avant clean install

--- Lancement.sh
+++ Lancement.sh
@@ -2,6 +2,7 @@
 
 #L'objectif de ce scirpt est de lancer une charge de travail avec l'ordonnancement EDF disponnible sur linux.
 # chrt -d -T <runtime> -D <deadline> -P <period> 0 <command> <command options>
-# gcc -o -pthread Workload ChargeDeTravail.c -lm
-echo "Compilation terminé"
+echo" gcc -o -pthread Workload Travail.c -lm"
+gcc -o -pthread Workload Travail.c -lm
+echo "------- Lancement du programme -----------"
 #./Workload
